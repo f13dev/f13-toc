27ee6e726bcc5c6b294b17a0c940aadb318fcd4f
James Valentine <jv@f13dev.com>
2021-10-15 22:37:29 +0100

    Feat: Recursive function for H1-6

--- controllers/control.php
+++ controllers/control.php
@@ -17,82 +17,6 @@ class Control
         return $string;
     }
 
-    public function find_and_replace($content)
-    {
-        if (!current_user_can('administrator')) {
-            return $content;
-        }
-        global $post;
-        $pattern_all = "/<\/?h[1-3](.*)>(.*)</i";
-        $pattern_h1 = "/<\/?h1(.*)>(.*)</i";
-        $pattern_h2 = "/<\/?h2(.*)>(.*)</i";
-        $pattern_h3 = "/<\/?h3(.*)>(.*)</i";
-        $count = 1;
-        $h1_count = 0;
-        $h2_count = 0;
-        $h3_count = 0;
-
-        $toc = array();
-
-        if (preg_match_all($pattern_all, $content, $header)) {
-            $i = 0;
-            foreach ($header[0] as $htag) {
-                if (!preg_match('/name=/i', $htag)) {
-                    if (preg_match($pattern_h1, $htag)) {
-                        $h1_count++;
-                        $h2_count = 1;
-                        $h3_count = 1;
-                        $content = str_replace($htag, '<h1 id="f13-toc-'.$h1_count.'" '.$header[1][$i].'>'.$header[2][$i].'<', $content);
-                        $toc[$h1_count] = array(
-                            'title' => $header[2][$i],
-                            'children' => array(),
-                            'level' => 'h1',
-                            'anchor' => 'f13-toc-'.$h1_count,
-                        );
-                    } else
-                    if (preg_match($pattern_h2, $htag)) {
-                        $h2_count++;
-                        $h3_count = 1;
-                        $content = str_replace($htag, '<h2 id="f13-toc-'.$h1_count.'-'.$h2_count.'" '.$header[1][$i].'>'.$header[2][$i].'<', $content);
-                        $toc[$h1_count]['children'][$h2_count] = array(
-                            'title' => $header[2][$i],
-                            'children' => array(),
-                            'level' => 'h2',
-                            'anchor' => 'f13-toc-'.$h1_count.'-'.$h2_count,
-                        );
-                    } else
-                    if (preg_match($pattern_h3, $htag)) {
-                        $h3_count++;
-                        $content = str_replace($htag, '<h3 id="f13-toc-'.$h1_count.'-'.$h2_count.'-'.$h3_count.'" '.$header[1][$i].'>'.$header[2][$i].'<', $content);
-                        $toc[$h1_count]['children'][$h2_count]['children'][$h3_count] = array(
-                            'title' => $header[2][$i],
-                            'level' => 'h3',
-                            'anchor' => 'f13-toc-'.$h1_count.'-'.$h2_count.'-'.$h3_count,
-                        );
-                    }
-                    $count++;
-                }
-                $i++;
-            }
-        }
-
-        $v = '';
-        if (!empty($toc)) {
-            $v .= '<div class="f13-toc">';
-                $v .= '<strong>'.__('Table of contents', 'f13-toc').'</strong>';
-                $v .= $this->_generate_list($toc);
-            $v .= '</div>';
-
-            $v = '<details class="f13-toc">';
-                $v .= '<summary>'.__('Table of contents', 'f13-toc').'</summary>';
-                $v .= $this->_generate_list($toc);
-            $v .= '</details>';
-        }
-
-        return $v.$content;
-
-    }
-
     public function _generate_list($toc, $v = '')
     {
         foreach ($toc as $key => $item) {
@@ -112,4 +36,88 @@ class Control
         }
         return ($parent) ? '<ol>'.$v.'</ol>' : $v;
     }
+
+    public function _pattern($level = '[1-6]')
+    {
+        return "/<\/?h$level(.*)>(.*)</i";
+    }
+
+    public function find_and_replace($content)
+    {
+        if (!current_user_can('administrator')) {
+            return $content;
+        }
+
+        $count = 1;
+        $h1_count = $h2_count = $h3_count = $h4_count = $h5_count = $h6_count = 0;
+        $toc = array();
+
+        if (preg_match_all($this->_pattern(), $content, $header)) {
+            $h = 0;
+            foreach ($header[0] as $htag) {
+                if (!preg_match('/name=/i', $htag)) {
+                    for($i = 1; $i <= 6; $i++) {
+                        if (preg_match($this->_pattern($i), $htag)) {
+                            $var = 'h'.$i.'_count';
+                            $$var++;
+                            $id = 'f13-toc';
+                            $keys = array();
+                            for ($j = 1; $j < $i; $j++) {
+                                $var = 'h'.$j.'_count';
+                                $id .= '-'.$$var;
+                                $keys[] = $$var;
+                                $keys[] = 'children';
+                            }
+                            $var = 'h'.$i.'_count';
+                            $id .= '-'.$$var;
+                            for ($k = 6; $k > $i; $k--) {
+                                $var = 'h'.$k.'_count';
+                                $$var = 1;
+                            }
+
+                            $content = str_replace($htag, '<h'.$i.' id="'.$id.'" '.$header[1][$h].'>'.$header[2][$h].'<', $content);
+                            switch ($i) {
+                                case '1':
+                                    $toc[$h1_count] = array('title' => $header[2][$h],'children' => array(),'level' => 'h1','anchor' => $id,);
+                                    break;
+                                case '2':
+                                    $toc[$h1_count]['children'][$h2_count] = array('title' => $header[2][$h],'children' => array(),'level' => 'h2','anchor' => $id,);
+                                    break;
+                                case '3':
+                                    $toc[$h1_count]['children'][$h2_count]['children'][$h3_count] = array('title' => $header[2][$h],'children' => array(),'level' => 'h3','anchor' => $id,);
+                                    break;
+                                case '4':
+                                    $toc[$h1_count]['children'][$h2_count]['children'][$h3_count]['children'][$h4_count] = array('title' => $header[2][$h],'children' => array(),'level' => 'h4','anchor' => $id,);
+                                    break;
+                                case '5':
+                                    $toc[$h1_count]['children'][$h2_count]['children'][$h3_count]['children'][$h4_count]['children'][$h5_count] = array('title' => $header[2][$h],'children' => array(),'level' => 'h5','anchor' => $id,);
+                                    break;
+                                case '6';
+                                    $toc[$h1_count]['children'][$h2_count]['children'][$h3_count]['children'][$h4_count]['children'][$h5_count]['children'][$h6_count] = array('title' => $header[2][$h],'children' => array(),'level' => 'h6','anchor' => $id,);
+                                    break;
+                            }
+                            $count++;
+                        }
+                    }
+                }
+                $h++;
+            }
+        }
+
+        $v = '';
+        if (!empty($toc)) {
+            $v .= '<div class="f13-toc">';
+                $v .= '<strong>'.__('Table of contents', 'f13-toc').'</strong>';
+                $v .= $this->_generate_list($toc);
+            $v .= '</div>';
+
+            $v = '<details class="f13-toc">';
+                $v .= '<summary>'.__('Table of contents', 'f13-toc').'</summary>';
+                $v .= $this->_generate_list($toc);
+            $v .= '</details>';
+        }
+
+        return $v.$content;
+    }
+
 }
