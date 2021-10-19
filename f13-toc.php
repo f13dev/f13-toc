7f1acdd84aa92a8b4b06787354ad21c0eea9655a
James Valentine <jv@f13dev.com>
2021-10-19 17:31:20 +0100

    Feat: Updating autoloader to comply with WP guidelines

--- f13-toc.php
+++ f13-toc.php
@@ -3,7 +3,7 @@
 Plugin Name: F13 TOC
 Plugin URI: https://f13.dev/wordpress-plugins/wordpress-plugin-table-of-contents/
 Description: Table of contents for pages and posts
-Version: 1.0
+Version: 1.0.1
 Author: Jim Valentine
 Author URI: https://f13.dev
 Text Domain: f13-toc
@@ -13,7 +13,7 @@ namespace F13\TOC;
 
 if (!function_exists('get_plugins')) require_once(ABSPATH.'wp-admin/includes/plugin.php');
 if (!defined('F13_TOC')) define('F13_TOC', get_plugin_data(__FILE__, false, false));
-if (!defined('F13_TOC_PATH')) define('F13_TOC_PATH', plugin_dir_path( __FILE__ ));
+if (!defined('F13_TOC_PATH')) define('F13_TOC_PATH', realpath(plugin_dir_path( __FILE__ )));
 if (!defined('F13_TOC_URL')) define('F13_TOC_URL', plugin_dir_url(__FILE__));
 
 class Plugin
@@ -33,13 +33,13 @@ class Plugin
             return;
         }
         $file = str_replace(__NAMESPACE__, '', $name);
-        $file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
+        $file = ltrim(str_replace('\\', DIRECTORY_SEPARATOR, $file), DIRECTORY_SEPARATOR);
         $file = plugin_dir_path(__FILE__).strtolower($file).'.php';
 
-        if (file_exists($file)) {
-            require_once $file;
+        if ($file !== realpath($file) || !file_exists($file)) {
+            wp_die('Class not found: '.htmlentities($name));
         } else {
-            die('Class not found: '.$name);
+            require_once $file;
         }
     }
 
